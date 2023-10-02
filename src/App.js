b445284feb34bb37ee9e5bb6fddc823113598272
Akash <kumawat.aakash515@gmail.com>
2023-10-02 10:24:15 +0530

    Project Image Gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import React from 'react';
 import './App.css';
+import Gallery from "./components/Gallery"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+
+
 
 function App() {
   return (
-    <div className="App">
-
-hello    </div>
+  <><div>
+<Navbar/>
+  <div className='rt' >
+  <h1 style={{textAlign:"center"}} >Hey User </h1>
+    <h2 style={{textAlign:"center"}} >Welcome To 4K iMAGES</h2>
+    <h3 style={{textAlign:"center" }}>We Provide The Premium Wallpapers</h3>
+  <Gallery/>
+  <Footer/>
+  </div>
+  </div>
+  </>
   );
 }
 
---
+++ src/components/Gallery.js
@@ -0,0 +1,235 @@
+import React from "react";
+import "./Gallery.css"
+
+
+
+
+
+const Gallery=()=>{
+
+   
+    return (
+       <><div className="rew">
+       
+       <div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=1" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+       
+  
+
+
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=2" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=3" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=4" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=5" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=6" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+
+</div>
+
+
+
+<div className="rew2">
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=7" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=8" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=9" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=10" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=111" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=122" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+ 
+</div>
+
+
+<div className="rew3">
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=121" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+       
+  
+
+
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=42" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=33" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=41" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=52" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=23" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+</div>
+
+<div className="rew4">
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=7" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=8" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=9" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=10" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=111" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+<div className="card" style={{width:"18rem"}}>
+  <img src="https://picsum.photos/300/180?random=122" style={{width:"18rem"}} alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Premium Wallpapers</h5>
+    <p className="card-text">We Provides The Real Content</p>
+    <a href="/" className="btn btn-primary">Download</a>
+  </div>
+</div>
+ 
+</div>
+       </>
+                         
+    )
+    
+}
+export default Gallery;
